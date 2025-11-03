ce027fe211c2901863d1217212a94cc2ab9279a4
Aulianurfitria15 <aulianurfitria17@gmail.com>
2025-11-03 11:27:52 +0700

    first commit

---
+++ models/music.js
@@ -0,0 +1,22 @@
+module.exports = (sequelize, DataTypes) => {
+    const music = sequelize.define('music', {
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+    },
+    Nama:{
+        type: DataTypes.STRING,
+    },
+    Author:{
+        type: DataTypes.STRING,
+    },
+    tahunTerbit:{
+        type: DataTypes.INTEGER,
+    },
+    Genre:{
+        type: DataTypes.STRING,
+    },
+    });
+    return music;
+};
